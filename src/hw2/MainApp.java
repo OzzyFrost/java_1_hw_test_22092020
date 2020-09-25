4b6230b61ff49cfd7d54f6b3983061bda7235548
FF <5kla@mail.ru>
2020-09-26 00:23:20 +0500

    hw1

---
+++ src/hw2/MainApp.java
@@ -0,0 +1,9 @@
+package hw2;
+
+public class MainApp {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("hello world!");
+        }
+    }
+}
